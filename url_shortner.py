c2c0389805588642537c52392c72da5d19abbce1
MargiShah18 <97111878+MargiShah18@users.noreply.github.com>
2024-05-14 18:00:16 +0530

    Add files via upload

---
+++ url_shortner.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pyshorteners as pyst
+import pyperclip
+
+shortner=pyst.Shortener()
+def copying():
+    pyperclip.copy(shorted_url)
+st.markdown("<h1 style='text-align: center;'>URL SHORTNER</h1>",unsafe_allow_html=True)
+form=st.form("Name")
+url=form.text_input("URL Here")
+s_btn=form.form_submit_button("SHORT")
+if s_btn:
+    shorted_url=shortner.tinyurl.short(url)
+    st.markdown("<h3>Shorted URL</h3>",unsafe_allow_html=True)
+    st.markdown(f"<h6 style='text-align: center;'>{shorted_url}</h6>",unsafe_allow_html=True)
+    st.button("Copy",on_click=copying)
